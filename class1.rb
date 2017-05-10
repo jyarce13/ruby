e63147ab933107e4b79ad741e02324e115bc82fb
jyarce13 <jyarce@MacBook-Pro-de-Theo.local>
2017-05-09 19:33:53 -0500

    Se sube class1.rb

---
+++ class1.rb
@@ -0,0 +1,18 @@
+puts 1, 'hola clas', 1.0
+puts 1
+puts 'hola clase'
+puts 1.0 # este metodo imprime 1.0 en la consola
+
+puts 'ingresa por favor tu edad:'
+edad = gets.chomp
+
+puts "tu edad es: #{edad}"
+
+
+numero1 = 45
+numero2 = 50
+numero3 = 100
+
+resultado = numero1 + numero2 + numero3
+
+puts "El resultado es: #{resultado}"
